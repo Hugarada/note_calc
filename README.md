cabdeaf06478d9a9d1651a3ec7fa6c3a0fe094ed
Hugo Miguel Trindade Parada <119022@Cooptecnica.local>
2022-01-12 10:29:37 +0000

    Finished for now

---
+++ README.md
@@ -0,0 +1,6 @@
+#Calculador automático da media de notas
+_Pode ser mais explorado mais tarde_
+
+Para saber a sua media, vá à pasta files e lá está o ficheiro `notas.txt`. Abra-o escreve o numero da nota linha por linha.
+
+A linha parecerá algo como isto ![Image](image.png "imagem")
--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-f1 = open("files/notas.txt", "r")
-f2 = open("files/notas_sem_atrasos.txt", "r")
-nota1 = []
-nota2 = []
+f = open("files/notas.txt", "r")
+nota = []
+nota_all = []
 soma1 = 0
 soma2 = 0
-for i in f1:
-    nota1.append(int(f1.readline()))
-for i in f2:
-    nota2.append(int(f2.readline()))
-for x in nota1:
-    soma1 += nota1[x]
-for x in nota2:
-    soma2 += nota2[x]
-soma1 = soma1 / len(nota1)
-soma2 = soma2 / len(nota2)
+ic = 0
+for i in f:
+    ic += 1
+    nota_all.append(int(f.readline()))
+    if nota_all[ic - 1] >= 10:
+        nota.append(nota_all[len(nota_all) - 1])
+for x in nota_all:
+    soma1 += nota_all[x]
+for j in nota:
+    soma2 += nota[j]
+soma1 = soma1 / (len(nota_all) - 1)
+soma2 = soma2 / (len(nota) - 1)
 print("O valor completo é: " + str(soma1) + "\nO valor sem os modulos em atraso é: " + str(soma2))
 print()
 input('Prima enter para fechar.')
